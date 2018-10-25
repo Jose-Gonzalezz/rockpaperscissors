663c459583ea3f4e381acc1702c29382d7875774
Jose Gonzalez <jose.gonzalezz0808@gmail.com>
2018-10-25 01:00:48 +0000

    added new veriables and created a random number checker

--- js/script.js
+++ js/script.js
@@ -9,8 +9,24 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
-$("shoot").click(function() {
-    var computerchoice="Rock"
-    $("userchoice").text("#input").val())
+$("#shoot").click(function() {
+    $("#userChoice").text($("#input").val())
+    $('#computerChoice').text(computerchoice)
     
-}
+    if(Math.random() > .9){
+    $("#computerChoice").html("Rock")}
+    else if(Math.random() > .6) {
+    $("#computerChoice").html("Paper") }
+    else if(Math.random() > .3){
+    $("#computerChoice").html("Scissors")
+    }
+});
+
+var computerchoice="";
+
+var userChoice="";
+
+var winner="";
+
+var randomNumber="0";
+
